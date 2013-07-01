f3713aec1ba234513e9d1ab72342b6a1e7cc2a11
ryan spangler <ryan.spangler@gmail.com>
2013-07-01 14:45:42 -0700

    releasing zalgo to the wild

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zalgo "0.1.0-SNAPSHOT"
+(defproject zalgo "6.6.6"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
