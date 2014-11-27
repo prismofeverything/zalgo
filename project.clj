40d39863d52fb84dda19874a8072afc8e77f308b
arrdem <me@arrdem.com>
2014-11-27 13:25:23 -0600

    Bump clojure.core dep, version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject zalgo "11.6.13"
+(defproject zalgo "11.6.14"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
